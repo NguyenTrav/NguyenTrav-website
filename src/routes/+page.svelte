c8772cff6679d394ecc97c16b22d76af0ae52fb6
Travis Nguyen <NguyenTrav@gmail.com>
2025-01-25 12:27:30 -0800

    hello world

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-4xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-blue-500 text-white text-center p-12">
+    <h1>does this finally work</h1>
+</div>
