f4e09c470e4a9bda82ecc7cc54d0ee2369f6b5a1
Travis Nguyen <NguyenTrav@gmail.com>
2025-01-27 22:15:05 -0800

    started structuring site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
-<h1 class="text-4xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="bg-blue-500 text-white text-center p-12">
-    <h1>does this finally work</h1>
-</div>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+    <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+        <div class="lg:flex lg:justify-between lg:gap-4">
+            <header class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
+                <h1>Travis Nguyen</h1>
+                <nav>
+                    <ul>
+                        <li>About</li>
+                        <li>Experience</li>
+                        <li>Projects</li>
+                    </ul>
+                </nav>
+                <ul>
+                    <li>Github</li>
+                    <li>Gmail</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </header>
+        
+            <main> 
+                <section>About</section>
+                <section>Experience</section>
+                <section>Projects</section>
+                <footer></footer>
+            </main>
+        </div> 
+    </div>
+</body>
