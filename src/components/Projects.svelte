d4f700b1ae1565e10c4f02502d10f6934ed79e6c
Travis Nguyen <NguyenTrav@gmail.com>
2025-02-21 15:55:12 -0800

    updated footer, font colors and hoves

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -11,7 +11,7 @@
        <ul class="group/list">
           <li class="mb-12">
              <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-emerald-950/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-green-950/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div class="z-10 sm:order-2 sm:col-span-6">
                    <h3>
                       <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-emerald-300 focus-visible:text-green-300  group/link text-base" href="https://nguyentrav.github.io/lights-out/" target="_blank" rel="noreferrer noopener" aria-label="Lights Out Game (opens in a new tab)">
@@ -33,7 +33,7 @@
           </li>
           <li class="mb-12">
              <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-emerald-950/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-green-950/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <div class="z-10 sm:order-2 sm:col-span-6">
                    <h3>
                       <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-green-300 focus-visible:text-green-300  group/link text-base" href="https://nguyentrav.github.io/todo-app/" target="_blank" rel="noreferrer noopener" aria-label="Todo List App(opens in a new tab)">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	let { children } = $props();
 	import '../app.css';
 	import Header from "../components/Header.svelte";
+	import mirror from "../lib/assets/mirror.jpg";
 	import { onMount } from "svelte";
    	onMount(async () => {
 
@@ -19,8 +20,8 @@
 	<div>
 	   <section>
 		  <!-- <div class="bg-red-500 sm:bg-orange-500 md:bg-yellow-500 lg:bg-green-500 xl:bg-teal-500">Media Query</div> -->
-		  <!-- <img class="mx-auto h-screen block sm:hidden md:block xl:hidden" src="./src/lib/assets/cliffjump.jpeg" alt="cliffjump"/>
-		  <img class="mx-auto h-screen md:hidden" src="./src/lib/assets/bonsai.jpeg" alt="cliffjump"/> -->
+		  <!-- <img class="mx-auto h-screen block sm:hidden md:block xl:hidden" src="{mirror}" alt="cliffjump"/> -->
+		  <img class="mx-auto" src="{mirror}" alt="cliffjump"/>
 		  <p class="coordinates font-thin tracking-tight text-center">48°27'32"N 122°36'28"W 2024.06.08</p>
 	   </section>
 	   <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
