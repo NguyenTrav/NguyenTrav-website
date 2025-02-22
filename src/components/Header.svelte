b5fcf72fea54443f0cc5a5a70d5bacc2cfa6e67e
Travis Nguyen <NguyenTrav@gmail.com>
2025-02-22 11:25:42 -0800

    updated gmail link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -35,7 +35,7 @@
              </a>
        </li>
        <li class="mr-5 shrink-0 text-xs">
-             <a class="block hover:text-slate-200" href="mailto:nguyentrav@gmail.com" target="_blank" rel="noreferrer noopener" aria-label="Gmail (opens in a new tab)" title="Gmail"><span class="sr-only">Gmail</span>
+             <a class="block hover:text-slate-200" href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=nguyentrav@gmail.com" target="_blank" rel="noreferrer noopener" aria-label="Gmail (opens in a new tab)" title="Gmail"><span class="sr-only">Gmail</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220 220" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M108,0C48.353,0,0,48.353,0,108s48.353,108,108,108s108-48.353,108-108S167.647,0,108,0z M156.657,60L107.96,98.498L57.679,60H156.657z M161.667,156h-109V76.259l50.244,38.11c1.347,1.03,3.34,1.545,4.947,1.545c1.645,0,3.073-0.54,4.435-1.616l49.374-39.276V156z"/></svg>
              </a>
        </li>
