d697f75c27a5f30c91c244e8e26717e8fb6b37b1
Travis Nguyen <NguyenTrav@gmail.com>
2025-02-25 10:55:38 -0800

    remove padding and shrink for xl media

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,9 +28,9 @@
 	<div>
 	   <section>
 		<!-- <div class="bg-red-500 sm:bg-orange-500 md:bg-green-500 lg:bg-blue-500 xl:bg-violet-500">Media Query</div> -->
-		  <enhanced:img class="mx-auto pt-4 md:hidden w-96" src="{cliffjumpsm}" alt="cliffjump" loading="lazy"/>		  
-		  <enhanced:img class="mx-auto pt-4 hidden md:block lg:hidden w-1/2" src="{cliffjumpmd}" alt="cliffjump" loading="lazy"/>
-		  <enhanced:img class="mx-auto pt-4 hidden lg:block lg:w-2/5 xl:w-1/3" src="{cliffjumplg}" alt="cliffjump" loading="lazy"/>
+		  <enhanced:img class="mx-auto md:hidden w-96" src="{cliffjumpsm}" alt="cliffjump" loading="lazy"/>		  
+		  <enhanced:img class="mx-auto hidden md:block lg:hidden w-1/2" src="{cliffjumpmd}" alt="cliffjump" loading="lazy"/>
+		  <enhanced:img class="mx-auto hidden lg:block lg:w-2/5 xl:w-1/4" src="{cliffjumplg}" alt="cliffjump" loading="lazy"/>
 		  <p class="coordinates font-thin tracking-tight text-center pt-2 md:pt-4">48°27'32"N 122°36'28"W 2024.06.08</p>
 	   </section>
 	   <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
